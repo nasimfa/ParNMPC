ad07560200b11bff0fa2d1fe7182ce3fc731f46c
nasimfa <133388728+nasimfa@users.noreply.github.com>
2023-06-15 12:47:38 -0400

    Update NMPC_Problem_Formulation.m

--- Quadrotor/NMPC_Problem_Formulation.m
+++ Quadrotor/NMPC_Problem_Formulation.m
@@ -3,60 +3,53 @@ addpath('../ParNMPC/')
 %% Formulate an OCP using Class OptimalControlProblem
 
 % Create an OptimalControlProblem object
-OCP = OptimalControlProblem(12,... % dim of inputs 
+OCP = OptimalControlProblem(3,... % dim of inputs 
                            12,... % dim of states 
-                            3,... % dim of parameters 
+                            1,... % dim of parameters 
                             48);  % N: num of discritization grids
 
 % Give names to x, u, p
-[X,dX,Y,dY,Z,dZ,Gamma,Beta,Alpha] = ...
+[omega_x , omega_y , omega_z , ta_x , eta_y , eta_z , etadot_x , etadot_y , etadot_z , phi , theta ,psi] = ...
     OCP.setStateName({'omega_x','omega_y','omega_z','eta_x','eta_y','eta_z','etadot_x','etadot_y','etadot_z','phi','theta','psi'});
-[a,omegaX,omegaY,omegaZ,slack] = ...
-    OCP.setInputName({'a','omegaX','omegaY','omegaZ','slack'});
-[XSP,YSP,ZSP] = ...
-    OCP.setParameterName({'XSP','YSP','ZSP'},[1 2 3]);
+[u1,u2,u3] = ...
+    OCP.setInputName({'u1','u2','u3'});
+[t] = ...
+    OCP.setParameterName({'t'},[1]);
 
 % Set the prediction horizon T
 OCP.setT(1);
 
 % Set the dynamic function f
 g = 9.81;
-f = [  2.74e-4 * x[4] - 0.0102 * x[3] - 6.84e-4 * x[5] - 1.53e-4 * x[6] + 1.65e-5 * x[7] - 2.74e-5 * x[8] + 0.00254 * u[0] - 8.76e-5 * u[1] - 2.01e-4 * u[2] + 6.79e-5 * x[6] * x[0] - 0.00689 * x[6] * x[1] + 3.5e-4 * x[7] * x[0] + 0.00387 * x[6] * x[2] + 0.00458 * x[7] * x[1] + 5.72e-4 * x[8] * x[0] + 0.00243 * x[7] * x[2] + 0.00181 * x[8] * x[1] + 0.00645 * x[8] * x[2] - 0.014 * x[0] * x[1] + 0.014 * x[0] * x[2] + 0.229 * x[1] * x[2];
-0.00346 * u[1] - 3.19e-4 * x[4] - 0.00214 * x[5] - 3.82e-5 * x[6] - 1.91e-5 * x[7] - 8.55e-5 * x[8] - 8.76e-5 * u[0] - 0.00255 * x[3] + 2.16e-5 * u[2] + 0.00759 * x[6] * x[0] + 3.33e-4 * x[6] * x[1] - 0.00591 * x[7] * x[0] - 0.0226 * x[6] * x[2] - 1.77e-4 * x[7] * x[1] - 0.00283 * x[8] * x[0] + 0.00442 * x[7] * x[2] - 4.64e-5 * x[8] * x[1] - 0.00403 * x[8] * x[2] + 0.00151 * x[0] * x[1] - 0.554 * x[0] * x[2] - 0.00789 * x[1] * x[2];
-7.48e-4 * x[4] - 0.0063 * x[3] + 0.00105 * x[5] - 9.46e-5 * x[6] + 4.49e-5 * x[7] + 4.21e-5 * x[8] - 2.01e-4 * u[0] + 2.16e-5 * u[1] + 0.00506 * u[2] - 0.00653 * x[6] * x[0] + 0.0333 * x[6] * x[1] - 0.00509 * x[7] * x[0] - 4.01e-4 * x[6] * x[2] - 0.00692 * x[7] * x[1] - 0.0127 * x[8] * x[0] - 1.73e-4 * x[7] * x[2] + 0.0054 * x[8] * x[1] - 5.25e-4 * x[8] * x[2] + 0.354 * x[0] * x[1] - 0.00345 * x[0] * x[2] - 0.0181 * x[1] * x[2];
-x[6];
-x[7];
-x[8];
-0.00491 * x[5] - 0.00302 * x[4] - 0.556 * x[3] - 0.00835 * x[6] - 1.81e-4 * x[7] + 1.96e-4 * x[8] - 0.016 * u[0] - 0.00398 * u[1] - 0.00985 * u[2] + 0.00405 * x[6] * x[0] - 0.0289 * x[6] * x[1] + 0.0166 * x[7] * x[0] + 0.00513 * x[6] * x[2] - 0.0143 * x[7] * x[1] + 0.0278 * x[8] * x[0] - 0.0211 * x[7] * x[2] - 0.0236 * x[8] * x[1] - 0.0355 * x[8] * x[2] - 0.689 * x[0] * x[1] + 0.637 * x[0] * x[2] - 1.44 * x[1] * x[2];
-0.00304 * x[5] - 0.0881 * x[4] - 0.0214 * x[3] - 3.22e-4 * x[6] - 0.00528 * x[7] + 1.22e-4 * x[8] + 0.00305 * u[0] - 0.00354 * u[1] + 0.00831 * u[2] - 0.0186 * x[6] * x[0] + 0.0473 * x[6] * x[1] - 0.00229 * x[7] * x[0] + 0.027 * x[6] * x[2] - 0.00562 * x[7] * x[1] - 0.018 * x[8] * x[0] - 0.00155 * x[7] * x[2] + 0.0116 * x[8] * x[1] + 0.0115 * x[8] * x[2] + 0.582 * x[0] * x[1] + 0.567 * x[0] * x[2] + 0.274 * x[1] * x[2];
-0.0126 * x[3] + 0.00109 * x[4] - 0.243 * x[5] + 1.88e-4 * x[6] + 6.56e-5 * x[7] - 0.00972 * x[8] - 0.00274 * u[0] - 0.00855 * u[1] + 0.00421 * u[2] - 0.0243 * x[6] * x[0] + 0.0334 * x[6] * x[1] + 0.0103 * x[7] * x[0] + 0.052 * x[6] * x[2] - 0.0101 * x[7] * x[1] - 0.00369 * x[8] * x[0] - 0.0138 * x[7] * x[2] + 0.00244 * x[8] * x[1] + 0.00256 * x[8] * x[2] + 0.295 * x[0] * x[1] + 1.37 * x[0] * x[2] - 0.246 * x[1] * x[2];
-x[0] + x[2] * cos(x[9]) * tan(x[10]) + x[1] * sin(x[9]) * tan(x[10]);
-x[1] * cos(x[9]) - 1.0 * x[2] * sin(x[9]);
-(x[2] * cos(x[9]) + x[1] * sin(x[9])) / cos(x[10])];
+f = [ 2.68e-4*eta_y - 0.00933*eta_x - 6.61e-4*eta_z - 1.36e-4*eta_dot_x + 1.52e-5*eta_dot_y - 3.53e-5*eta_dot_z + 0.00255*u1 - 1.06e-4*u2 - 2.41e-4*u3 + 7.89e-5*eta_dot_x*omega_x - 0.00705*eta_dot_x*omega_y + 3.98e-4*eta_dot_y*omega_x + 0.00394*eta_dot_x*omega_z + 0.00457*eta_dot_y*omega_y + 6.88e-4*eta_dot_z*omega_x + 0.00221*eta_dot_y*omega_z + 0.00186*eta_dot_z*omega_y + 0.00647*eta_dot_z*omega_z - 0.0281*omega_x*omega_y + 0.027*omega_x*omega_z + 0.229*omega_y*omega_z + 2.98e-4*omega_x^2 - 0.0262*omega_y^2 + 0.0259*omega_z^2;
+0.00347*u2 - 3.3e-4*eta_y - 0.00205*eta_z - 2.95e-5*eta_dot_x - 1.87e-5*eta_dot_y - 1.1e-4*eta_dot_z - 1.06e-4*u1 - 0.00202*eta_x - 1.5e-4*u3 + 0.00768*eta_dot_x*omega_x - 7.39e-4*eta_dot_x*omega_y - 0.00567*eta_dot_y*omega_x - 0.0226*eta_dot_x*omega_z + 1.12e-5*eta_dot_y*omega_y - 0.00253*eta_dot_z*omega_x + 0.00427*eta_dot_y*omega_z - 2.56e-4*eta_dot_z*omega_y - 0.00414*eta_dot_z*omega_z + 0.0247*omega_x*omega_y - 0.555*omega_x*omega_z - 0.0206*omega_y*omega_z + 0.0143*omega_x^2 + 6.11e-4*omega_y^2 - 0.015*omega_z^2; 
+7.68e-4*eta_y - 0.00543*eta_x + 0.00116*eta_z - 7.92e-5*eta_dot_x + 4.35e-5*eta_dot_y + 6.2e-5*eta_dot_z - 2.41e-4*u1 - 1.5e-4*u2 + 0.00508*u3 - 0.00681*eta_dot_x*omega_x + 0.0333*eta_dot_x*omega_y - 0.00441*eta_dot_y*omega_x + 6.61e-4*eta_dot_x*omega_z - 0.00679*eta_dot_y*omega_y - 0.0125*eta_dot_z*omega_x - 4.1e-4*eta_dot_y*omega_z + 0.00547*eta_dot_z*omega_y - 4.32e-4*eta_dot_z*omega_z + 0.355*omega_x*omega_y - 0.0275*omega_x*omega_z - 9.14e-4*omega_y*omega_z - 0.0158*omega_x^2 + 0.0176*omega_y^2 - 0.00181*omega_z^2;
+eta_dot_x;
+eta_dot_y;
+eta_dot_z;
+0.00439*eta_z - 0.00297*eta_y - 0.515*eta_x - 0.00752*eta_dot_x - 1.68e-4*eta_dot_y + 2.34e-4*eta_dot_z - 0.0158*u1 - 0.00343*u2 - 0.0092*u3 + 0.00436*eta_dot_x*omega_x - 0.0253*eta_dot_x*omega_y + 0.0142*eta_dot_y*omega_x + 0.00195*eta_dot_x*omega_z - 0.0149*eta_dot_y*omega_y + 0.0258*eta_dot_z*omega_x - 0.0188*eta_dot_y*omega_z - 0.0235*eta_dot_z*omega_y - 0.0355*eta_dot_z*omega_z - 0.615*omega_x*omega_y + 0.594*omega_x*omega_z - 1.45*omega_y*omega_z + 0.0139*omega_x^2 + 0.13*omega_y^2 - 0.144*omega_z^2;
+0.003*eta_z - 0.0904*eta_y - 0.019*eta_x - 2.76e-4*eta_dot_x - 0.00512*eta_dot_y + 1.6e-4*eta_dot_z + 0.0029*u1 - 0.00357*u2 + 0.00833*u3 - 0.0183*eta_dot_x*omega_x + 0.0475*eta_dot_x*omega_y - 0.00167*eta_dot_y*omega_x + 0.0268*eta_dot_x*omega_z - 0.00562*eta_dot_y*omega_y - 0.0177*eta_dot_z*omega_x - 0.00183*eta_dot_y*omega_z + 0.0117*eta_dot_z*omega_y + 0.0112*eta_dot_z*omega_z + 0.536*omega_x*omega_y + 0.497*omega_x*omega_z + 0.305*omega_y*omega_z - 0.0393*omega_x^2 - 0.00405*omega_y^2 + 0.0433*omega_z^2;
+0.0109*eta_x + 0.00116*eta_y - 0.231*eta_z + 1.59e-4*eta_dot_x + 6.59e-5*eta_dot_y - 0.0123*eta_dot_z - 0.00279*u1 - 0.00865*u2 + 0.00489*u3 - 0.0249*eta_dot_x*omega_x + 0.0376*eta_dot_x*omega_y + 0.00999*eta_dot_y*omega_x + 0.0523*eta_dot_x*omega_z - 0.0108*eta_dot_y*omega_y - 0.00493*eta_dot_z*omega_x - 0.0134*eta_dot_y*omega_z + 0.00313*eta_dot_z*omega_y + 0.00273*eta_dot_z*omega_z + 0.267*omega_x*omega_y + 1.33*omega_x*omega_z - 0.205*omega_y*omega_z - 0.0493*omega_x^2 + 0.0425*omega_y^2 + 0.00676*omega_z^2
+omega_x + omega_z*cos(phi)*tan(theta) + omega_y*sin(phi)*tan(theta);
+omega_y*cos(phi) - 1.0*omega_z*sin(phi);
+(omega_z*cos(phi) + omega_y*sin(phi))/cos(theta)];
 OCP.setf(f);
 OCP.setDiscretizationMethod('Euler');
 
 % Set the cost function L
-Q = diag([10, 1, 10, 1, 10, 1, 1, 1, 1]);
-R = diag([0.1, 0.1, 0.1, 0.1]);
+Q = diag([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+R = diag([0, 0, 0]);
 xRef = [0, 0, 0, 0, 0, 0, 0, 0, 0,0.1, 0, 0];
 
 uRef = [0;0;0];
 L =    0.5*(OCP.x-xRef).'*Q*(OCP.x-xRef)...
      + 0.5*(OCP.u(1:3)-uRef).'*R*(OCP.u(1:3)-uRef)...
-     + 1e5*slack^2;
+    ;
 OCP.setL(L);
 
 % Set the linear constraints G(u,x,p)>=0
-G =-[OCP.u(1:4) - [11;1;1; 1];...
-    -OCP.u(1:4) - [0;1;1;1];...
-    -slack;...
-     Gamma - slack - 0.2;...
-    -Gamma - slack - 0.2;...
-     Beta  - slack - 0.2;...
-    -Beta  - slack - 0.2;...
-     Alpha - slack - 0.2;...
-    -Alpha - slack - 0.2];
+G =-[OCP.u(1:3) - [10;10;10];...
+    -OCP.u(1:3) - [-10;-10;-10]];
 OCP.setG(G);
 
 % Generate necessary files
@@ -74,7 +67,7 @@ nmpcSolver.codeGen();
 %% Solve the very first OCP for a given initial state and given parameters using Class OCPSolver
 
 % Set the initial state
-x0 =   [1;0;1;0;1;0;0;0;0];
+x0 =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 % Set the parameters
 dim      = OCP.dim;
@@ -107,20 +100,23 @@ ParNMPCGlobalVariable.solutionInitGuess = solution;
 plant = DynamicSystem(4,9,0);
 
 % Give names to x, u
-[X,dX,Y,dY,Z,dZ,Gamma,Beta,Alpha] = ...
-    plant.setStateName({'X','dX','Y','dY','Z','dZ','Gamma','Beta','Alpha'});
-[a,omegaX,omegaY,omegaZ] = ...
-    plant.setInputName({'a','omegaX','omegaY','omegaZ'});
+[omega_x , omega_y , omega_z , ta_x , eta_y , eta_z , etadot_x , etadot_y , etadot_z , phi , theta ,psi] = ...
+    plant.setStateName({'omega_x','omega_y','omega_z','eta_x','eta_y','eta_z','etadot_x','etadot_y','etadot_z','phi','theta','psi'});
+[u1,u2,u3] = ...
+    plant.setInputName({'u1','u2','u3'});
 g = 9.81;
-fPlant = [  dX;...
-            a*(cos(Gamma)*sin(Beta)*cos(Alpha) + sin(Gamma)*sin(Alpha));...
-            dY;...
-            a*(cos(Gamma)*sin(Beta)*sin(Alpha) - sin(Gamma)*cos(Alpha));... 
-            dZ;...
-            a*cos(Gamma)*cos(Beta) - g;... 
-           (omegaX*cos(Gamma) + omegaY*sin(Gamma))/cos(Beta);... 
-           -omegaX*sin(Gamma) + omegaY*cos(Gamma);... 
-            omegaX*cos(Gamma)*tan(Beta) + omegaY*sin(Gamma)*tan(Beta) + omegaZ];
+fPlant = [  2.68e-4*eta_y - 0.00933*eta_x - 6.61e-4*eta_z - 1.36e-4*eta_dot_x + 1.52e-5*eta_dot_y - 3.53e-5*eta_dot_z + 0.00255*u1 - 1.06e-4*u2 - 2.41e-4*u3 + 7.89e-5*eta_dot_x*omega_x - 0.00705*eta_dot_x*omega_y + 3.98e-4*eta_dot_y*omega_x + 0.00394*eta_dot_x*omega_z + 0.00457*eta_dot_y*omega_y + 6.88e-4*eta_dot_z*omega_x + 0.00221*eta_dot_y*omega_z + 0.00186*eta_dot_z*omega_y + 0.00647*eta_dot_z*omega_z - 0.0281*omega_x*omega_y + 0.027*omega_x*omega_z + 0.229*omega_y*omega_z + 2.98e-4*omega_x^2 - 0.0262*omega_y^2 + 0.0259*omega_z^2;
+0.00347*u2 - 3.3e-4*eta_y - 0.00205*eta_z - 2.95e-5*eta_dot_x - 1.87e-5*eta_dot_y - 1.1e-4*eta_dot_z - 1.06e-4*u1 - 0.00202*eta_x - 1.5e-4*u3 + 0.00768*eta_dot_x*omega_x - 7.39e-4*eta_dot_x*omega_y - 0.00567*eta_dot_y*omega_x - 0.0226*eta_dot_x*omega_z + 1.12e-5*eta_dot_y*omega_y - 0.00253*eta_dot_z*omega_x + 0.00427*eta_dot_y*omega_z - 2.56e-4*eta_dot_z*omega_y - 0.00414*eta_dot_z*omega_z + 0.0247*omega_x*omega_y - 0.555*omega_x*omega_z - 0.0206*omega_y*omega_z + 0.0143*omega_x^2 + 6.11e-4*omega_y^2 - 0.015*omega_z^2; 
+7.68e-4*eta_y - 0.00543*eta_x + 0.00116*eta_z - 7.92e-5*eta_dot_x + 4.35e-5*eta_dot_y + 6.2e-5*eta_dot_z - 2.41e-4*u1 - 1.5e-4*u2 + 0.00508*u3 - 0.00681*eta_dot_x*omega_x + 0.0333*eta_dot_x*omega_y - 0.00441*eta_dot_y*omega_x + 6.61e-4*eta_dot_x*omega_z - 0.00679*eta_dot_y*omega_y - 0.0125*eta_dot_z*omega_x - 4.1e-4*eta_dot_y*omega_z + 0.00547*eta_dot_z*omega_y - 4.32e-4*eta_dot_z*omega_z + 0.355*omega_x*omega_y - 0.0275*omega_x*omega_z - 9.14e-4*omega_y*omega_z - 0.0158*omega_x^2 + 0.0176*omega_y^2 - 0.00181*omega_z^2;
+eta_dot_x;
+eta_dot_y;
+eta_dot_z;
+0.00439*eta_z - 0.00297*eta_y - 0.515*eta_x - 0.00752*eta_dot_x - 1.68e-4*eta_dot_y + 2.34e-4*eta_dot_z - 0.0158*u1 - 0.00343*u2 - 0.0092*u3 + 0.00436*eta_dot_x*omega_x - 0.0253*eta_dot_x*omega_y + 0.0142*eta_dot_y*omega_x + 0.00195*eta_dot_x*omega_z - 0.0149*eta_dot_y*omega_y + 0.0258*eta_dot_z*omega_x - 0.0188*eta_dot_y*omega_z - 0.0235*eta_dot_z*omega_y - 0.0355*eta_dot_z*omega_z - 0.615*omega_x*omega_y + 0.594*omega_x*omega_z - 1.45*omega_y*omega_z + 0.0139*omega_x^2 + 0.13*omega_y^2 - 0.144*omega_z^2;
+0.003*eta_z - 0.0904*eta_y - 0.019*eta_x - 2.76e-4*eta_dot_x - 0.00512*eta_dot_y + 1.6e-4*eta_dot_z + 0.0029*u1 - 0.00357*u2 + 0.00833*u3 - 0.0183*eta_dot_x*omega_x + 0.0475*eta_dot_x*omega_y - 0.00167*eta_dot_y*omega_x + 0.0268*eta_dot_x*omega_z - 0.00562*eta_dot_y*omega_y - 0.0177*eta_dot_z*omega_x - 0.00183*eta_dot_y*omega_z + 0.0117*eta_dot_z*omega_y + 0.0112*eta_dot_z*omega_z + 0.536*omega_x*omega_y + 0.497*omega_x*omega_z + 0.305*omega_y*omega_z - 0.0393*omega_x^2 - 0.00405*omega_y^2 + 0.0433*omega_z^2;
+0.0109*eta_x + 0.00116*eta_y - 0.231*eta_z + 1.59e-4*eta_dot_x + 6.59e-5*eta_dot_y - 0.0123*eta_dot_z - 0.00279*u1 - 0.00865*u2 + 0.00489*u3 - 0.0249*eta_dot_x*omega_x + 0.0376*eta_dot_x*omega_y + 0.00999*eta_dot_y*omega_x + 0.0523*eta_dot_x*omega_z - 0.0108*eta_dot_y*omega_y - 0.00493*eta_dot_z*omega_x - 0.0134*eta_dot_y*omega_z + 0.00313*eta_dot_z*omega_y + 0.00273*eta_dot_z*omega_z + 0.267*omega_x*omega_y + 1.33*omega_x*omega_z - 0.205*omega_y*omega_z - 0.0493*omega_x^2 + 0.0425*omega_y^2 + 0.00676*omega_z^2
+omega_x + omega_z*cos(phi)*tan(theta) + omega_y*sin(phi)*tan(theta);
+omega_y*cos(phi) - 1.0*omega_z*sin(phi);
+(omega_z*cos(phi) + omega_y*sin(phi))/cos(theta)];
 
 % Set the dynamic function f
 plant.setf(fPlant); % same model 
